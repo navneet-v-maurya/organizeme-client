6f7a199a5c5fe440f937c233ab705b81a8f241b5
theHarshRohit <harshrohit0700@gmail.com>
2023-10-30 22:47:12 +0530

    starting with header comp

--- src/components/common/headers/Header.tsx
+++ src/components/common/headers/Header.tsx
@@ -1,9 +1,45 @@
 import './Header.css'
-
+import{CgProfile} from 'react-icons/cg'
+import {BsFillBellFill} from 'react-icons/bs'
 
 
 const Header = () => {
-  return <div className="head">Header</div>;
+  return <div className="head">
+    <div>logo</div>
+    <div><input type="search" /></div>
+    <div className='rightheader'>
+      <div className='notification'>
+        <div><BsFillBellFill/></div>
+        <div>Notification</div>
+      </div>
+      <div className='user'>
+      <div ><CgProfile/></div>
+        <div>username</div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Header;
+//left
+
+// 1. Logo.
+
+
+
+
+// middle
+
+// 1. Search Bar.
+
+
+
+
+
+// right
+
+// 1. Notification.
+
+// 2. Profile Photo.
+
+// 2. User Name.
